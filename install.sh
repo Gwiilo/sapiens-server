b6c8b649769cc14e6bb9b44e4405406e4a78e74c
DecDuck <declanahofmeyr@gmail.com>
2024-04-27 19:46:03 +1000

    removed sudo

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 chmod +x sapiens.sh start.sh
-sudo add-apt-repository multiverse
-sudo dpkg --add-architecture i386
-sudo apt update -y
-sudo apt install steamcmd -y
+add-apt-repository multiverse
+dpkg --add-architecture i386
+apt update -y
+apt install steamcmd -y
 steamcmd +force_install_dir ./server +login anonymous +app_update 2886350 validate +quit
 mkdir ~/.steam/sdk64
 cd ~/.steam/sdk64/
