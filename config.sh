90fc072aecb5522094f5a541b98eebb1439af2e6
DecDuck <declanahofmeyr@gmail.com>
2024-04-27 19:44:22 +1000

    fixed lol

--- config.sh
+++ config.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Location of where the server executable is located.  If you followed the readme instructions this probably doesn't need to be changed.
-GAME_DIR="/home/YOUR_USERNAME/.local/share/Steam/steamcmd/sapiens"
+GAME_DIR="./server"
 
 # Location of where the world is.  You will need to see what was generated and set it here and replace the CAPS_VALUES to match what you have.
-WORLD_DIR="/home/YOUR_USERNAME/.local/share/majicjungle/sapiens/players/PLAYER_NUMBER_FOLDER_NAME/worlds/WORLD_NUMBER_FOLDER_NAME"
+# WORLD_DIR="/home/YOUR_USERNAME/.local/share/majicjungle/sapiens/players/PLAYER_NUMBER_FOLDER_NAME/worlds/WORLD_NUMBER_FOLDER_NAME"
 
 # The folder where the backup command will send your world archive.  This folder was created with the git package, just change as necessary. 
-BACKUP_DIR="/home/YOUR_USERNAME/sapiens-server/world_backups"
+# BACKUP_DIR="/home/YOUR_USERNAME/sapiens-server/world_backups"
 
 # World Name.  This is the name used in the game that shows up on the map as well as the server listing.
-WORLD_NAME="My New World"
+WORLD_NAME="You forgot to change this name you dummy"
 
 # Network settings.  These are the defaults, change as necessary.  Please note there is 1 additional port that needs to be opened on your
 # firewall that will be +1 above the UDP_PORT.  So using the defaults you would have to open 16161 and 16162 for UDP and 16168 for TCP 
--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 chmod +x sapiens.sh start.sh
 sudo add-apt-repository multiverse
 sudo dpkg --add-architecture i386
-sudo apt update
-sudo apt install steamcmd
-steamcmd +runscript ~/sapiens-server/steamupdate.txt
+sudo apt update -y
+sudo apt install steamcmd -y
+steamcmd +force_install_dir ./server +login anonymous +app_update 2886350 validate +quit
 mkdir ~/.steam/sdk64
 cd ~/.steam/sdk64/
 ln -s ../steam/steamcmd/linux64/steamclient.so steamclient.so
